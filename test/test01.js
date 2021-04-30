ec4a5f5d987a6712bcdb67c72d360f0498d956d0
gonzalogcontacto <gonzalogcontacto@gmail.com>
2021-04-30 15:25:11 +0200

    * test01 passed

--- test/test01.js
+++ test/test01.js
@@ -1,4 +1,19 @@
 function clasificaElementos(index) {
-    return [0,0,0];
+    
+    let positiveArr = []
+    let negativeArr = []
+    let zeroArr= []
+    
+    index.forEach((item) => {
+        if (item < 0) 
+            negativeArr.push(item)
+        else if(item === 0)
+            zeroArr.push(item)
+        else 
+            positiveArr.push(item)
+    })
+
+    return [(positiveArr.length/index.length).toFixed(4), (negativeArr.length/index.length).toFixed(4), (zeroArr.length/index.length).toFixed(4)]
 }
+
 module.exports = clasificaElementos;
